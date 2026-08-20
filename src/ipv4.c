6c694ecaa6e9eec35e6c325162e188bd8113d9d6
dikshyant <adhikarybad@gmail.com>
2026-08-20 14:22:55 +0545

    Ipv4 Parsed

--- src/ipv4.c
+++ src/ipv4.c
@@ -13,8 +13,14 @@ void parse_ipv4(const unsigned char *packet)
     uint16_t total_length = ntohs(ip->total_length);
     uint16_t identification = ntohs(ip->identification);
     uint16_t flags_fragment_offset = ntohs(ip->flags_fragment_offset);
+    uint8_t flags = (flags_fragment_offset >> 13) & 0x07;
+    uint16_t fragment_offset = flags_fragment_offset & 0x1FFF;
     uint8_t protocol = ip->protocol;
     uint16_t checksum = ntohs(ip->checksum);
+    char source_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &ip->source_ip, source_ip, sizeof(source_ip));
+    char destination_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &ip->destination_ip, destination_ip, sizeof(destination_ip));
 
     printf("IPV4 packet! \n");
     printf("Version: %u\n", version);
@@ -22,9 +28,10 @@ void parse_ipv4(const unsigned char *packet)
     printf("Time to Live : %u\n", ttl);
     printf("Total Length : %u\n", total_length);
     printf("Identification : %u\n", identification);
-    printf("Flag Fragment offset: %u\n", flags_fragment_offset);
+    printf("Flag : %u\n", flags);
+    printf("Fragment Offset : %u\n", fragment_offset);
     printf("Protocol: %u\n", protocol); //useful for next parser selector, 6 = tcp, 17 = udp, 1 = icmp
     printf("Checksum: 0x%04x\n", checksum);
-
-
+    printf("Source IP: %s\n", source_ip);
+    printf("Destination IP: %s\n", destination_ip);
 }
