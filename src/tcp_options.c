fbd95616c1779cdd0ecc61cc366749d7bef57dc1
dikshyant <adhikarybad@gmail.com>
2026-08-24 22:39:43 +0545

    tcp options part 1 completed

--- src/tcp_options.c
+++ src/tcp_options.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include "tcp_options.h"
+#include <arpa/inet.h>
 
 void parse_tcp_options ( const unsigned char *options, uint8_t options_length){
     uint8_t i = 0;
@@ -34,6 +36,50 @@ void parse_tcp_options ( const unsigned char *options, uint8_t options_length){
         break;
             }
         printf("  Option Kind: %u, Length: %u\n", kind, length);
+
+        switch (kind){
+            case 2: {
+                if (length != 4){
+                    printf ("Malformed MSS option \n");
+                    break;
+                }
+
+                uint16_t mss;
+
+                memcpy(&mss, &options[i + 2], sizeof(mss));
+
+                mss = ntohs(mss);
+                printf (" MSS : %u bytes \n", mss);
+                break;
+            }
+            case 3: {
+                if (length != 3){
+                    printf ("Malformed window Scale option \n");
+                    break;
+                }
+
+                uint8_t winscale;
+
+                memcpy (&winscale, &options[i + 2], sizeof(winscale));
+                printf(" Window scale : %u \n ", winscale);
+
+                break;
+                }
+
+            case 4 : {
+                if (length != 2){
+                    printf ("Malformed Selective Ack option \n ");
+                    break;
+                }
+                printf("    SACK Permitted\n");
+                break;
+
+            }
+
+            default:
+            printf("Unknown options \n");
+            break;
+        }
 
         i = i + length;
     }
